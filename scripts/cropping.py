96e1c29ae0326e6348bbb871241cdb46c72b19e9
xintong <xintongyang@foxmail.com>
2020-12-18 00:37:15 +0000

    transformation matrix

--- scripts/cropping.py
+++ scripts/cropping.py
@@ -10,32 +10,38 @@ polygon = np.array([
     # [-0.7, -0.3, 2.5],
     # [-0.3, -0.3, 2.5]
     [0.0, 0.0, 0.0],
-    [0.0, 0.0, 0.1],
-    [0.0, 0.0, 0.15],
-    [0.0, 0.0, 0.2],
-    [0.0, 0.0, 0.25],
-    [0.0, 0.0, 0.3],
-    [0.0, 0.0, 0.35],
-    [0.1, 0.0, 0.0],
-    [0.15, 0.0, 0.0],
-    [0.2, 0.0, 0.0],
-    [0.25, 0.0, 0.0],
-    [0.3, 0.0, 0.0],
-    [0.0, 0.1, 0.0],
-    [0.0, 0.15, 0.0],
-    [0.0, 0.2, 0.0],
-    [0.0, 0.25, 0.0],
-    [0.0, 0.3, 0.0],
-    [0.0, 0.35, 0.0]
+    [0.0, 0.0, 0.02],
+    [0.0, 0.0, 0.04],
+    [0.0, 0.0, 0.06],
+    [0.0, 0.0, 0.08],
+    [0.0, 0.0, 0.10],
+    [0.0, 0.0, 0.12],
+    [0.0, 0.0, 0.14],
+    [0.02, 0.0, 0.0],
+    [0.04, 0.0, 0.0],
+    [0.06, 0.0, 0.0],
+    [0.08, 0.0, 0.0],
+    [0.10, 0.0, 0.0],
+    [0.0, 0.02, 0.0],
+    [0.0, 0.04, 0.0],
+    [0.0, 0.06, 0.0],
+    [0.0, 0.08, 0.0],
+    [0.0, 0.10, 0.0],
+    [0.0, 0.12, 0.0]
 ])
 polygon = o3d.geometry.PointCloud(
         points=o3d.utility.Vector3dVector(polygon.tolist())
     )
 polygon.paint_uniform_color([1, 0, 0])
 
-translation_matrix = -np.array([-0.526, 0.162, 1.090]).reshape((3, 1))   # xyz
-rotation_matrix = o3d.geometry.get_rotation_matrix_from_quaternion(np.array([0.003, -0.995, 0.086, 0.035]).reshape((4, 1)))  # wxyz
-# rotation_matrix = np.transpose(rotation_matrix)
+# homogeneous transformation matrix from Robot frame to Camera frame
+translation_matrix = np.array([-0.456, 0.150, 1.060]).reshape((3, 1))   # xyz
+rotation_matrix = o3d.geometry.get_rotation_matrix_from_quaternion(np.array([-0.030, -0.991, 0.093, 0.085]).reshape((4, 1)))  # xyzw
+transformation_matrix = np.append(rotation_matrix, translation_matrix, axis=1)
+transformation_matrix = np.append(transformation_matrix, np.array([[0, 0, 0, 1]]), axis=0)
+
+# homogeneous transformation matrix from Camera frame to Robot frame
+transformation_matrix_ = np.linalg.inv(transformation_matrix)
 
 
 def display_inlier_outlier(cloud, ind):
@@ -49,13 +55,22 @@ def display_inlier_outlier(cloud, ind):
 
 
 if __name__ == "__main__":
+    # Robot frame
+    mesh = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2)
+    # Camera frame, transformed into Robot frame
+    mesh_t = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2).transform(transformation_matrix_)
+
     print("Load a ply point cloud, print it, and render it")
-    pcd = o3d.io.read_point_cloud(cwd + "/../objects/cube_xyz_4.ply")
+    pcd = o3d.io.read_point_cloud(cwd + "/../objects/part_xyz_4.ply")
     # print(np.asarray(pcd.points))
     # o3d.visualization.draw_geometries([pcd])
 
     print("Downsample the point cloud with a voxel of 0.02")
     voxel_down_pcd = pcd.voxel_down_sample(voxel_size=0.005)
+    voxel_down_pcd = voxel_down_pcd.transform(transformation_matrix_)
+    voxel_down_pcd.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=30))
+
     # voxel_down_pcd = o3d.geometry.voxel_down_sample(pcd, voxel_size=2.0)
     # o3d.visualization.draw_geometries([voxel_down_pcd])
     # print(voxel_down_pcd)
@@ -65,7 +80,7 @@ if __name__ == "__main__":
     # print(np.asarray(voxel_down_pcd.points))
 
     print("Polygon visualisation")
-    o3d.visualization.draw_geometries([voxel_down_pcd, polygon])
+    o3d.visualization.draw_geometries([mesh, mesh_t, voxel_down_pcd])
 
     # print("Load a polygon volume and use it to crop the original point cloud")
     # vol = o3d.visualization.read_selection_polygon_volume(cwd + "/json/crop.json")
