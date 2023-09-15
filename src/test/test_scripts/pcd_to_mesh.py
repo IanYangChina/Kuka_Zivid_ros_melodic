1c02ccaeb57273f0f0a645643382ec7a3f593d10
Xintong <xintongyang1994@foxmail.com>
2023-09-15 11:41:48 +0100

    surface reconstruction pipeline mostly finished testing

--- src/test/test_scripts/pcd_to_mesh.py
+++ src/test/test_scripts/pcd_to_mesh.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
-import open3d as o3d  # >= 0.12.0
-import pymeshfix
+import open3d as o3d  # >= 0.14.1
+from pymeshfix import _meshfix
 import pyvista as pv
 
 script_path = os.path.dirname(os.path.abspath(__file__))
@@ -11,44 +11,28 @@ bounding_box_array = np.load(os.path.join(script_path, 'transformation_matrices'
 bounding_box = o3d.geometry.OrientedBoundingBox.create_from_points(points=o3d.utility.Vector3dVector(bounding_box_array))
 world_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0, 0, 0])
 pcd = o3d.io.read_point_cloud(pcd_path).crop(bounding_box)
+pcd = pcd.voxel_down_sample(voxel_size=0.003)  # 0.003 is a good value for downsampling
 
-_, ind = pcd.remove_statistical_outlier(nb_neighbors=25, std_ratio=1.5)
-outliner = pcd.select_by_index(ind, invert=True).paint_uniform_color([1, 0, 0])
-pcd = pcd.select_by_index(ind).paint_uniform_color([0, 0.5, 0.5])
-
-_, ind = pcd.remove_radius_outlier(nb_points=15, radius=0.004)
-outliner = outliner + pcd.select_by_index(ind, invert=True).paint_uniform_color([0, 1, 0])
-pcd = pcd.select_by_index(ind).paint_uniform_color([0, 0.5, 0.5])
-
-pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.006, max_nn=30))
+pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.005, max_nn=30))
 pcd.orient_normals_consistent_tangent_plane(50)
-o3d.io.write_point_cloud(os.path.join(script_path, '..', 'objects', 'pcd_to_mesh', 'pcd_0_processed.ply'), pcd)
-o3d.visualization.draw_geometries([world_frame, pcd, bounding_box, outliner], width=800, height=800, point_show_normal=False)
 
-radii = [0.003, 0.006]
+radii = [0.003]
 mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
-
-o3d.visualization.draw_geometries([pcd, world_frame, mesh, bounding_box],
-                                  width=800, height=800,
-                                  mesh_show_back_face=True,
-                                  mesh_show_wireframe=True)
 
 mesh_path = os.path.join(script_path, '..', 'objects', 'pcd_to_mesh', 'mesh_0.obj')
 o3d.io.write_triangle_mesh(mesh_path, mesh)
-
-# todo: looking for better mesh repairing tools
-
 mesh = pv.read(mesh_path)
-mf = pymeshfix.MeshFix(mesh)
-mf.repair()
-repaired = mf.mesh
-repaired_mesh_path = os.path.join(script_path, '..', 'objects', 'pcd_to_mesh', 'mesh_0_repaired.ply')
-repaired.save(repaired_mesh_path)
+mesh_path = os.path.join(script_path, '..', 'objects', 'pcd_to_mesh', 'mesh_0.ply')
+mesh.save(mesh_path)
 
-repaired_mesh = o3d.io.read_triangle_mesh(repaired_mesh_path)
-o3d.visualization.draw_geometries([pcd, world_frame, repaired_mesh, bounding_box],
+mesh_to_fix = _meshfix.PyTMesh()
+mesh_to_fix.load_file(mesh_path)
+mesh_to_fix.fill_small_boundaries()
+repaired_mesh_path = os.path.join(script_path, '..', 'objects', 'pcd_to_mesh', 'mesh_0_repaired.obj')
+mesh_to_fix.save_file(repaired_mesh_path)
+
+repaired_mesh_0 = o3d.io.read_triangle_mesh(repaired_mesh_path)
+o3d.visualization.draw_geometries([pcd, world_frame, repaired_mesh_0, bounding_box],
                                   width=800, height=800,
                                   mesh_show_back_face=True,
                                   mesh_show_wireframe=True)
-repaired_mesh_path = os.path.join(script_path, '..', 'objects', 'pcd_to_mesh', 'mesh_0_repaired.obj')
-o3d.io.write_triangle_mesh(repaired_mesh_path, repaired_mesh)
--- src/test/test_scripts/test_doma_load_mesh.py
+++ src/test/test_scripts/test_doma_load_mesh.py
@@ -6,25 +6,24 @@ from doma.envs import ClayEnv
 script_path = os.path.dirname(os.path.realpath(__file__))
 mesh_file_path = os.path.join(script_path, '..', 'objects', 'pcd_to_mesh', 'mesh_0_repaired.obj')
 env = ClayEnv(ptcl_density=2e7, horizon=500,
-              mesh_file=mesh_file_path, material_id=3, voxelise_res=500, initial_pos=(0.25, 0.25, 0.15))
+              mesh_file=mesh_file_path, material_id=3, voxelise_res=1080, initial_pos=(0.25, 0.25, 0.15))
 env.reset()
 # done = False
 # while not done:
 #     env.render(mode='human')
 # env.mpm_env.apply_agent_action_p(np.array((1., 1., 1.)))
 frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0, 0, 0])
-real_pcd = o3d.io.read_point_cloud(os.path.join(script_path, '..', 'objects', 'pcd_to_mesh', 'pcd_0_processed.ply')).paint_uniform_color([0.5, 0.5, 0.5])
+real_pcd = o3d.io.read_point_cloud(os.path.join(script_path, '..', 'objects', 'pcd_to_mesh', 'pcd_0.ply')).paint_uniform_color([1, 0, 0])
 xyz_real = np.asarray(real_pcd.points)
 
 x, x_ = env.render(mode='point_cloud')
 x -= np.array([0.25, 0.25, 0.15])
 x += env.original_center
 obj_vec = o3d.utility.Vector3dVector(x)
-obj_pcd = o3d.geometry.PointCloud(obj_vec).paint_uniform_color([1, 0, 0])
+obj_pcd = o3d.geometry.PointCloud(obj_vec).paint_uniform_color([0.5, 0.5, 0.5])
 xyz_sim = np.asarray(obj_pcd.points)
 
 d = real_pcd.compute_point_cloud_distance(obj_pcd)
 print(np.asarray(d).mean())
 
 o3d.visualization.draw_geometries([frame, obj_pcd, real_pcd], width=800, height=600)
-
