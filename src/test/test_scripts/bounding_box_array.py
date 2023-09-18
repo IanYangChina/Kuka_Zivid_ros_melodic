c0ef09cc86b83e29718ae5629352d751823150ae
xintong <xintongyang1994@foxmail.com>
2023-09-18 16:08:28 +0100

    calibrating new camera base

--- src/test/test_scripts/bounding_box_array.py
+++ src/test/test_scripts/bounding_box_array.py
@@ -8,10 +8,10 @@ workspace_bounding_box_array = np.array(
      [-0.5, 0.1, 0.2],
      [-0.33, -0.1, 0.2],
      [-0.5, -0.1, 0.2],
-     [-0.33, 0.1, 0.084],
-     [-0.5, 0.1, 0.084],
-     [-0.33, -0.1, 0.084],
-     [-0.5, -0.1, 0.084]]
+     [-0.33, 0.1, 0.001],
+     [-0.5, 0.1, 0.001],
+     [-0.33, -0.1, 0.001],
+     [-0.5, -0.1, 0.001]]
 )
 np.save(os.path.join(script_path, 'transformation_matrices', 'reconstruction_bounding_box_array_in_base.npy'),
         workspace_bounding_box_array)
